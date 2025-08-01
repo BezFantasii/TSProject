34faaca75c384f1068abd0425dd8fa6da89cceac
BezFantasii <selibovaa2008@gmail.com>
2025-08-01 22:45:47 +0300

    my first commit

---
+++ TSProjectNuxt/app/components/UI/dataField.vue
@@ -0,0 +1,11 @@
+<template>
+    <InputText type="tel" v-model="inputValue"/>
+</template>
+
+<script setup lang="ts">
+import { InputText } from 'primevue';
+</script>
+
+<style>
+
+</style>
---
+++ TSProjectNuxt/app/components/dataModule.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="DataModule">
+    
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.dataModule{
+  margin: 10px;
+  border-radius: 20px;
+  box-sizing: border-box;
+}
+</style>
